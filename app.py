11af8448961809f186ab117cc7ab61b1d55359a7
glev1 <guilevi880@gmail.com>
2021-12-30 21:13:28 -0300

    Change app port

--- app.py
+++ app.py
@@ -11,25 +11,25 @@ def change(amount):
                    10: "Dimes",
                    5: "Nickels",
                    1: "Pennies"}
-    
+
     # Divide the amount in cents by the biggest coin value, record the number
     # of coins that evenly divides and the remainder.
     coin = coins.pop() # Biggest coin value
     num, rem = divmod(int(amount*100), coin)
-    
+
     # Append coin type and number that had no remainder
     res.append({num:coin_lookup[coin]})
-    
+
     # While there is still remainder, continue adding coins to the result
     while rem > 0:
         coin = coins.pop()
         num, rem = divmod(rem, coin)
-        if num: 
+        if num:
             if coin in coin_lookup:
                 res.append({num:coin_lookup[coin]})
-    
+
     return res
-    
+
 @app.route('/')
 def hello():
     """Return a HTTP greeting"""
@@ -42,11 +42,6 @@ def changeroute(dollar, cents):
     amount = f"{dollar}.{cents}"
     result = change(float(amount))
     return jsonify(result)
-    
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
-    
 
-    
-    
-    
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080 , debug=True)
